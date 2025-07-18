ae7a971f65dec52ca186b4379cab060647f6741d
wochoa94 <ochoa.werner94@gmail.com>
2025-07-18 12:06:06 -0400

    Update vite.config.ts to include @babel/core

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['@babel/core'],
     exclude: ['lucide-react'],
   },
 });
