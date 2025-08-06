703139df258670b60560e529db4d3f4a455403e7
wochoa94 <ochoa.werner94@gmail.com>
2025-08-05 20:55:47 -0600

    Updated logos.ts

--- src/theme/logos.ts
+++ src/theme/logos.ts
@@ -1,6 +1,6 @@
 // src/theme/logos.ts
 export const logos = {
-  default: '/vite.svg', // Path to your default logo
+  default: 'https://www.figma.com/design/X6Q3jtH9wIIszPM8GHmDbR/Flotaris?node-id=0-1&t=31fBRNk1AZDZC8gT-1', // Path to your default logo
   customerA: 'https://via.placeholder.com/150x50?text=CustomerA+Logo', // Example placeholder for Customer A
   // Add more logo paths for other themes
 };
