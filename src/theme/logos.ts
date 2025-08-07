8d20e4cbc21e53ddc830f651d57311794cdf9c94
wochoa94 <ochoa.werner94@gmail.com>
2025-08-06 21:55:18 -0600

    Add Flotaris logo and remove text from navigation

--- src/theme/logos.ts
+++ src/theme/logos.ts
@@ -1,6 +1,6 @@
 // src/theme/logos.ts
 export const logos = {
-  default: 'https://www.figma.com/design/X6Q3jtH9wIIszPM8GHmDbR/Flotaris?node-id=0-1&t=31fBRNk1AZDZC8gT-1', // Path to your default logo
+  default: '/Frame 79.png', // Path to your default logo
   customerA: 'https://via.placeholder.com/150x50?text=CustomerA+Logo', // Example placeholder for Customer A
   // Add more logo paths for other themes
 };
