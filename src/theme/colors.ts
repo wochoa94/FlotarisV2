c469364338eb02f77f69e6c2a960fccbfc1c1f80
wochoa94 <ochoa.werner94@gmail.com>
2025-08-07 22:49:30 -0600

    Implement Flotaris official color palette

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,33 +1,33 @@
 // src/theme/colors.ts
 export const themes = {
   default: {
-    '--color-primary': '#073459', // blue-600
-    '--color-primary-hover': '#094C84', // blue-700
-    '--color-primary-400': '#60A5FA', // lighter blue for active background
-    '--color-primary-focus-ring': '#266FC8', // blue-500
-    '--color-secondary': '#F28325', // gray-600
-    '--color-secondary-hover': '#EEB17E', // gray-700
-    '--color-secondary-700': '#D97706', // darker orange for active text/icon
-    '--color-secondary-focus-ring': '#D26000', // gray-500
-    '--color-success': '#A9D6C0', // green-500
-    '--color-success-hover': '#32674E', // green-600
-    '--color-success-focus-ring': '#64B48E', // green-400
-    '--color-warning': '#F5C870', // yellow-500
-    '--color-warning-hover': '#F8DAA0', // yellow-600
-    '--color-warning-focus-ring': '#F0AC29', // yellow-300
-    '--color-danger': '#EBADAD', // red-500
-    '--color-danger-hover': '#D75B5B', // red-600
-    '--color-danger-focus-ring': '#CD3232', // red-400
-    '--color-info': '#E2F2FF', // blue-500
-    '--color-info-hover': '#B8DEFE', // blue-600
-    '--color-info-focus-ring': '#2C94EB', // blue-400
+    '--color-primary': '#073459', // Primary 900
+    '--color-primary-hover': '#094C84', // Primary 800/Hover
+    '--color-primary-400': '#266FC8', // Primary 700
+    '--color-primary-focus-ring': '#2C94EB', // Info Focus Pressed
+    '--color-secondary': '#F28325', // Secondary 700
+    '--color-secondary-hover': '#EEB17E', // Secondary 600/Hover
+    '--color-secondary-700': '#D26000', // Secondary 800
+    '--color-secondary-focus-ring': '#D26000', // Secondary 800
+    '--color-success': '#A9D6C0', // Success 600
+    '--color-success-hover': '#32674E', // Success 800/Hover
+    '--color-success-focus-ring': '#64B48E', // Success 700
+    '--color-warning': '#F5C870', // Warning 700
+    '--color-warning-hover': '#F8DAA0', // Warning 600/Hover
+    '--color-warning-focus-ring': '#F0AC29', // Warning 800
+    '--color-danger': '#EBADAD', // Danger 600
+    '--color-danger-hover': '#D75B5B', // Danger 700/Hover
+    '--color-danger-focus-ring': '#CD3232', // Danger 800
+    '--color-info': '#E2F2FF', // Info
+    '--color-info-hover': '#B8DEFE', // Info Hover
+    '--color-info-focus-ring': '#2C94EB', // Info Focus Pressed
     '--color-background': '#FAFAFA', // gray-50
     '--color-background-alt': '', // white
     '--color-text-default': '#073459', // gray-900
-    '--color-text-secondary': '#B8DEFE', // gray-500
-    '--color-border': '#EEEFF3', // gray-200
-    '--color-input-border': '#C1C4D7', // gray-300
-    '--color-input-placeholder': '#9A9EB4', // gray-400
+    '--color-text-secondary': '#266FC8', // Primary 700
+    '--color-border': '#EEEFF3', // Color Border
+    '--color-input-border': '#C1C4D7', // Input Border
+    '--color-input-placeholder': '#9A9EB4', // Placeholder
   },
   customerA: {
     '--color-primary': '#8b5cf6', // purple-500
